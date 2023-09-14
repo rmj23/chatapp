35df5111b77b773e317021d60856f785f0b4aa1c
Ruben <ruben@publicinput.com>
2023-09-14 14:54:27 -0400

    initial socket io server files

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('message', (message) => {
+    console.log(message);
+    io.emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running on port ' + (process.env.PORT || 3000));
+});
